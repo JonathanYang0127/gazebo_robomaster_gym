2177926778f718b9fdd1522ef3e9daa778fd32d0
Jonathan Yang <jydumplings@gmail.com>
2019-11-08 10:17:05 -0800

    Add pausing to environment

--- src/robomaster_env.py
+++ src/robomaster_env.py
@@ -4,21 +4,34 @@ import rospy
 from roborts_msgs.msg import TwistAccel, GimbalAngle
 from std_msgs.msg import Empty
 from keyboard.msg import Key
+from gazebo_connection import GazeboConnection
+import time
 
 class RobomasterEnv:
     def __init__(self):
+        #Set up publishers for motion
         self.pub_cmd_vel = rospy.Publisher('cmd_vel_acc', TwistAccel, queue_size=1)
         self.cmd_vel_keyboard = TwistAccel();
         self.pub_gimbal_angle = rospy.Publisher('cmd_gimbal_angle', GimbalAngle, queue_size=1)
         self.cmd_gimbal_keyboard = GimbalAngle();
+        
+        #Set up gazebo connection
+        self.gazebo = GazeboConnection()
+        self.gazebo.pauseSim()
 
+        #Amount of time running per timestep
+        self.running_step = 0.01
 
-    def step(self):
-        self.cmd_vel_keyboard.twist.linear.x = 1
-        self.cmd_vel_keyboard.twist.linear.y = 1
+    def step(self, action):
+        self.gazebo.unpauseSim()
+        #self.cmd_vel_keyboard.twist.linear.x = 1
+        #self.cmd_vel_keyboard.twist.linear.y = 1
+        self.cmd_vel_keyboard.twist.angular.z = -1
         
         self.pub_cmd_vel.publish(self.cmd_vel_keyboard)
         self.pub_gimbal_angle.publish(self.cmd_gimbal_keyboard)
+        time.sleep(self.running_step)
+        self.gazebo.pauseSim()
        
 
     def switch_robot(self, robot_num):
@@ -32,9 +45,10 @@ class RobomasterEnv:
         self.pub_cmd_vel = rospy.Publisher(robot_name + '/cmd_vel_acc', TwistAccel, queue_size=1)
         self.pub_gimbal_angle = rospy.Publisher(robot_name + '/cmd_gimbal_angle', GimbalAngle, queue_size=1)
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     rospy.init_node('gym_env_node')
     env = RobomasterEnv()
-    for i in range(100):
-        env.step()
+    for i in range(1000):
+        env.step(1)
+        time.sleep(0.001)
     rospy.spin()
