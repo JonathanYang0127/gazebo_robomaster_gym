f279e5a5e4e10a4a1b571c49b6fbda1400f3f299
Sean Chen <00schen@berkeley.edu>
2020-01-10 15:55:24 -0500

    line_cross cases expanded and tested

--- robomaster_gym/misc/utils.py
+++ robomaster_gym/misc/utils.py
@@ -1,33 +1,55 @@
 
 import math
 
-def counterClockwise(ax, ay, bx, by, cx, cy):
-    return (cy - ay) * (bx - ax) > (by - ay) * (cx - ax)
-
-# assumes (x3, y3) <-> (x4, y4) is either parallel or perpendicular to X-axis
-def lines_cross(x1, y1, x2, y2, x3, y3, x4, y4):
-    if x3 == x4:
-        if x1 == x2 or (x1 - x3) * (x2 - x3) > 0:
-            return False # colinear counts as no cross due to buffer
-        y = (x3 - x1) / (x2 - x1) * (y2 - y1) + y1
-        return (y3 - y) * (y4 - y) < 0
-    elif y3 == y4:
-        if y1 == y2 or (y1 - y3) * (y2 - y3) > 0:
-            return False
-        x = (y3 - y1) / (y2 - y1) * (x2 - x1) + x1
-        return (x3 - x) * (x4 - x) < 0
+def orientation(xp, yp, xq, yq, xr, yr):
+    """
+    This determines if points p, q, and r are colinear(0,pi),
+    clockwise(-1) or counter-clockwise(1) by finding
+    magnitude of k-vector in cross-product with p as center
+    point. If colinear, this will return the angle <qpr.
+    """
+    cross = (xq-xp)*(yr-yp) - (xr-xp)*(yq-yp)
+    dot = (xq-xp)*(xr-xp) + (yr-yp)*(yq-yp)
+    if cross < 0:
+        return -1
+    elif cross > 0:
+        return 1
+    elif dot > 0:
+        return 0
     else:
-        # general case; we still ignore colinear cases
-        return counterClockwise(x1, y1, x3, y3, x4, y4) != counterClockwise(x2, y2, x3, y3, x4, y4) and \
-            counterClockwise(x1, y1, x2, y2, x3, y3) != counterClockwise(x1, y1, x2, y2, x4, y4)
+        return math.pi
+
+
+def lines_cross(x1, y1, x2, y2, x3, y3, x4, y4):
+    """
+    Consider the pair of triangles formed by orientations of
+    either point on one line with both points on the other:
+    say that the pair is opposing if the orientations are
+    different. Then for lines ((x1,y1),(x2,y2) and
+    ((x3,y3),(x4,y4)) to cross, either both pairs of triangles
+    are opposing, the lines share vertices or one line
+    contains the other. Assumes length > 0
+    """
+    a1 = orientation(x1,y1, x3,y3, x4,y4)
+    a2 = orientation(x2,y2, x3,y3, x4,y4)
+    b1 = orientation(x3,y3, x1,y1, x2,y2)
+    b2 = orientation(x4,y4, x1,y1, x2,y2)
+    return ((a1 != a2 and b1 != b2)\
+        or ((x1 == x3 and y1 == y3) or (x2 == x3 and y2 == y3)\
+            or (x1 == x4 and y1 == y4) or (x2 == x4 and y2 == y4))\
+        or ((a1 == math.pi and a2 == math.pi)\
+            or b1 == math.pi and b2 == math.pi))
 
 def distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def angleTo(x1, y1, x2, y2):
+    assert not (x1 == 0 and y1 == 0) and not (x2 == 0 and y2 == 0), "neither point should be the origin"
     if x1 == x2:
         if y1 < y2:
             return math.pi / 2
+        elif y1 == y2:
+            return 0
         return math.pi * 3 / 2
     dx, dy = x2 - x1, y2 - y1
     rawDeg = math.atan(dy / dx)
@@ -42,3 +64,25 @@ def angleBetween(x1, y1, x2, y2, x3, y3):
 def angleDiff(a1, a2):
     diff = abs(a1 - a2)
     return min(diff, math.pi * 2 - diff)
+
+if __name__ == "__main__":
+    print(orientation(1,1, 2,1, 1,2)) # -1
+    print(orientation(1,1, 1,0, 2,1)) # 1
+    print(orientation(1,1, 2,1, 0,1)) # pi
+    print(orientation(1,1, 2,1, 3,1)) # 0
+    print(lines_cross(1,1, 2,1, 3,1, 4,1)) # False
+    print(lines_cross(1,1, 3,1, 2,2, 3,2)) # False
+    print(lines_cross(1,1, 3,1, 2,0, 3,0)) # False
+    print(lines_cross(2,2, 3,2, 1,1, 3,1)) # False
+    print(lines_cross(2,0, 3,0, 1,1, 3,1)) # False
+    print(lines_cross(1,1, 3,1, 2,2, 2,3)) # False
+    print(lines_cross(1,1, 3,1, 2,0, 2,-1)) # False
+    print(lines_cross(1,1, 3,1, 2,0, 2,2)) # True
+    print(lines_cross(1,1, 3,1, 2,0, 2,1)) # True
+    print(lines_cross(1,1, 3,1, 2,1, 2,2)) # True
+    print(lines_cross(1,1, 3,1, 2,1, 4,1)) # True
+    print(lines_cross(1,1, 3,1, 2,1, 0,1)) # True
+    print(lines_cross(1,1, 4,1, 2,1, 3,1)) # True
+    print(lines_cross(1,1, 2,1, 2,1, 3,1)) # True
+    print(lines_cross(1,1, 2,1, 1,1, 2,1)) # True
+
