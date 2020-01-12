bd7600bdc6f6ebcdaba35c88e88babe988226b65
Sean Chen <00schen@berkeley.edu>
2020-01-11 22:56:04 -0500

    sub_games use gazebo_connection to set_model_state

--- scripts/closequarter_test.py
+++ scripts/closequarter_test.py
@@ -4,10 +4,11 @@ import roslaunch
 
 if __name__ == '__main__': 
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
     cli_args1 = ['roborts_sim','multi_robot.launch']
     cli_args2 = ['roborts_bringup','roborts_gazebo.launch','gui:=true']
-    roslaunch_file1 = roslaunch.rlutil.resolve_launch_arguments(cli_args1)
-    roslaunch_file2 = roslaunch.rlutil.resolve_launch_arguments(cli_args2)
+    roslaunch_file1 = roslaunch.rlutil.resolve_launch_arguments(cli_args1)[0]
+    roslaunch_file2 = roslaunch.rlutil.resolve_launch_arguments(cli_args2)[0]
     roslaunch_args2 = cli_args2[2:]
     launch_files = [roslaunch_file1,(roslaunch_file2,roslaunch_args2)]
     launch = roslaunch.parent.ROSLaunchParent(uuid, launch_files)
